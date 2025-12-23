f9373c45938e38f412e1da09184d6c1a69614612
jlbuild <juliabuildbot@gmail.com>
2025-12-23 14:19:17 +0000

    FFMPEG_jll build 8.0.1+0

--- src/FFMPEG_jll.jl
+++ src/FFMPEG_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("FFMPEG")
-JLLWrappers.@generate_main_file("FFMPEG", UUID("b22a6f82-2f65-5046-a5b2-351ab43fb4e5"))
+JLLWrappers.@generate_main_file("FFMPEG", Base.UUID("b22a6f82-2f65-5046-a5b2-351ab43fb4e5"))
 end  # module FFMPEG_jll
